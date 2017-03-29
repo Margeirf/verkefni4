e6146905773e1d4cd82368511f04e718e4450421
Aron Davíð Bjarkason Clausen <aroncl332@nemi.tskoli.is>
2017-03-29 09:59:28 +0000

    lagaði css style

--- event01.php
+++ event01.php
@@ -3,7 +3,7 @@
 
     <div class="containerEvent">
         <div class="InfoEvent">
-            <h2>Fun event name</h2>
+            <h2>Fun party</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pulvinar mi rhoncus blandit mattis. Fusce mattis malesuada purus, ut dapibus nisi consequat dapibus. Maecenas a lectus aliquam, sagittis erat non, rutrum elit. Donec congue nunc sed quam egestas, eu congue lorem lacinia. Sed tempor arcu a finibus pulvinar. Integer lobortis lorem at vestibulum hendrerit. Praesent vel facilisis odio. Integer ultricies luctus arcu nec fringilla.</p>
         </div>
         <div class="skraning">
